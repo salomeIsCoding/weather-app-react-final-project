c725c6bb268b3467ae3bcb39ad1a8a12874105fc
salomeIsCoding <160015952+salomeIsCoding@users.noreply.github.com>
2024-08-26 16:42:07 +0200

    Added html content to Weather

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Weather from './Weather';
 
 function App() {
   return (
     <div className="App">
         <div className='container'>
-        <h1>Hello</h1>
+        <Weather/>
         <p>This project was coded by <a href='https://github.com/salomeIsCoding' target='_blank' rel="noreferrer">Salomé Erard</a> and is open-sourced on <a href='https://github.com/salomeIsCoding/weather-app-react-final-project' target='_blank' rel="noreferrer">Github </a></p>
         </div>
     </div>
